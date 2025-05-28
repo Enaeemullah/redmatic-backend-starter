029a05c0cb393bada1ab125597519f8531ebd8ab
Naeem Ullah <enaeemullah@gmail.com>
2025-05-29 01:47:47 +0500

    Update ItemController.java

--- src/main/java/com/redmatic/starterkit/inventory/controller/ItemController.java
+++ src/main/java/com/redmatic/starterkit/inventory/controller/ItemController.java
@@ -1,10 +1,13 @@
 package com.redmatic.starterkit.inventory.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redmatic.starterkit.constants.AppConstants;
 import com.redmatic.starterkit.inventory.dto.ItemRequest;
 import com.redmatic.starterkit.inventory.dto.ItemResponse;
 import com.redmatic.starterkit.inventory.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,22 +15,18 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping(AppConstants.API_BASE_PATH + "/items")
+@RequestMapping(AppConstants.API_BASE_PATH + "items")
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ItemResponse> createItem(
-            @RequestPart("item") ItemRequest request,
-            @RequestPart("image") MultipartFile image
-    ) {
+            @RequestPart("item") String itemJson,
+            @RequestPart(value = "image", required = false) MultipartFile image
+    ) throws JsonProcessingException {
+        ItemRequest request = new ObjectMapper().readValue(itemJson, ItemRequest.class);
         return ResponseEntity.ok(itemService.addItem(request, image));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<ItemResponse>> getItems() {
-        return ResponseEntity.ok(itemService.getAllItems());
     }
 }
